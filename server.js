30d75dbff55c8d61f5890f051cc32720675e68b4
renskebingen <renske.bingen@gmail.com>
2026-03-09 13:15:37 +0100

    database toegevoegd

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+require("dotenv").config()
+
 const express = require('express')
+
+const { MongoClient, ServerApiVersion } = require("mongodb")
+
 const app = express()
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri)
 
 app.set("view engine", "ejs")
 
@@ -7,7 +16,17 @@ app.use(express.static('static'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+async function connectDB() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 })
+    console.log("✅ Connected to MongoDB")
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err)
+  }
+}
 
+connectDB()
 
 app.listen(2000, () => {
   console.log('Server is running on http://localhost:2000')
