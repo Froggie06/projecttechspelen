cc6a08df116cdddbb561832ba54b82f383a6533a
renskebingen <renske.bingen@gmail.com>
2026-04-03 23:20:03 +0200

    linters gefixt

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "no-duplicate-selectors": true,     // zelfde selector 2x gebruiken verbieden
+    "no-empty-source": true,            // lege CSS bestanden verbieden
+    "selector-no-qualifying-type": true, // geen 'div.container', alleen '.container'
+    "color-named": "never",               // geen kleurnamen gebruiken, alleen hex of rgb(a)
+    "selector-id-pattern": null,            // toestaan van id selectors
+  }
 };
